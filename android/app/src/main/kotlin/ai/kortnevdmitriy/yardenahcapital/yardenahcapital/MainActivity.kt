2af95b80347392f0f7c43832bfdab916111630c8
kortnevdmitriy <kortnevdmitriy@outlook.com>
2018-01-10 12:13:29 +0300

    Yardenah Capital first commit.

---
+++ android/app/src/main/kotlin/ai/kortnevdmitriy/yardenahcapital/yardenahcapital/MainActivity.kt
@@ -0,0 +1,13 @@
+package ai.kortnevdmitriy.yardenahcapital.yardenahcapital
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity() : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
